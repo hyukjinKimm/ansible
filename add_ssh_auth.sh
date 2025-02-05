0bf028467bd16b1b3d83037cd76847a18bb25f1b
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-02-05 19:40:27 +0900

    Update add_ssh_auth.sh

--- add_ssh_auth.sh
+++ add_ssh_auth.sh
@@ -2,6 +2,8 @@
 
 #ssh key 생성
 sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.10
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.11
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.12
 sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.20
 sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.30
 sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.40
