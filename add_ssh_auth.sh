7e04d177536943c3a084c80db5d06672c793d4fe
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-02-06 15:22:01 +0900

    Update add_ssh_auth.sh

--- add_ssh_auth.sh
+++ add_ssh_auth.sh
@@ -1,11 +1,11 @@
 #! /bin/bash bash
 
 #ssh key 생성
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.10
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.11
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.12
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.20
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.30
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.40
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.122
-sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.123
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.10 "exit"
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.11 "exit"
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.12 "exit"
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.20 "exit"
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.30 "exit"
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.40 "exit"
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.122 "exit"
+sshpass -p 1234 ssh -T -o StrictHostKeyChecking=no root@192.168.0.123 "exit"
