8a6b310822d8ec6cad99b458dd8589ee32f631e4
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-02-06 15:48:08 +0900

    Update clean_all.sh

--- clean_all.sh
+++ clean_all.sh
@@ -1,4 +1,4 @@
 #! /bin/bash 
 
-ansible-playbook clean_cluster.yml -vvv -k --ask-pass --ask-become-pass > clean_cluster.log
-ansible-playbook clean_lb.yml -vvv -k --ask-pass --ask-become-pass > clean_lb.log
+ansible-playbook clean_cluster.yml -vvv -k > clean_cluster.log
+ansible-playbook clean_lb.yml -vvv -k > clean_lb.log
