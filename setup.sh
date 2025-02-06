540a05f89eddaec00eb750def50c45c1c8fcd96f
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-02-06 15:26:43 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash 
 
+sh ansible-playbook ansible_env_ready.yml -vvv -k > ansible_env_ready.log
 sh add_ssh_auth.sh
 sh ansible-playbook init_lb.yml -vvv -k > init_lb.log
 sh ansible-playbook init_PrimaryMaster.yml -vvv -k > init_PrimaryMaster.log
