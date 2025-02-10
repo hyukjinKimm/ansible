c5960e1ca9c03ea80f5df41bf5afe12c18ffb298
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2025-02-10 19:26:14 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #! /bin/bash 
 
-sshpass -p '1234' ansible-playbook ansible_env_ready.yml -vvv -k > ansible_env_ready.log
+ansible-playbook ansible_env_ready.yml
 sh add_ssh_auth.sh
-sshpass -p '1234' ansible-playbook init_lb.yml -vvv -k > init_lb.log
-sshpass -p '1234' ansible-playbook init_PrimaryMaster.yml -vvv -k > init_PrimaryMaster.log
-sshpass -p '1234' ansible-playbook init_SecondaryMaster.yml -vvv -k > init_SecondaryMaster.log
-sshpass -p '1234' ansible-playbook init_workers.yml -vvv -k > init_workers.log
+ansible-playbook init_lb.yml -vvv -k -u myuser  --ask-become-pass  >  init_lb.log
+ansible-playbook init_PrimaryMaster.yml -vvv -k -u myuser  --ask-become-pass  >  init_PrimaryMaster.log
+ansible-playbook init_SecondaryMaster.yml -vvv -k -u myuser  --ask-become-pass  >  init_SecondaryMaster.log
+ansible-playbook init_workers.yml -vvv -k  -u myuser --ask-become-pass  >  init_workers.log
+
+#sshpass -p '1234' ansible-playbook ansible_env_ready.yml -vvv -k > ansible_env_ready.log
+#sh add_ssh_auth.sh
+#sshpass -p '1234' ansible-playbook init_lb.yml -vvv -k > init_lb.log
+#sshpass -p '1234' ansible-playbook init_PrimaryMaster.yml -vvv -k > init_PrimaryMaster.log
+#sshpass -p '1234' ansible-playbook init_SecondaryMaster.yml -vvv -k > init_SecondaryMaster.log
+#sshpass -p '1234' ansible-playbook init_workers.yml -vvv -k > init_workers.log
 
